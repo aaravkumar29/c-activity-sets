25d2981651015cdb23849719233afe27bb8ff6e7
aarav <aaravk.bsc23@rvu.edu.in>
2024-01-29 10:52:19 +0530

    first commit

---
+++ problem01.c
@@ -0,0 +1,7 @@
+// 1. Write a C program to print your name.
+#include <stdio.h>
+int main()
+{
+    printf("Aarav Kumar");
+    return 0;
+}
---
+++ problem02.c
@@ -0,0 +1,8 @@
+//2. Write a C program to add two numbers.
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter 2 numbers: ");
+    scanf(%d)
+}
