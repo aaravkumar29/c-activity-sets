4b663494076f064c4a24ef5940142cdc735ba799
aarav <aaravk.bsc23@rvu.edu.in>
2024-01-31 00:16:05 +0530

    test

--- problem06.c
+++ problem06.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 
-int input(int *a, int *b, int *c);
+void input(int *a, int *b, int *c);
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
 
 int main()
 {
-
+    int a, b, c, largest;
+    input(&a, &b, &c);
+    compare(a, b, c, &largest);
+    output(a, b, c, largest);
+    return 0;
 }
-int input(int *a, int *b, int *c)
+
+void input(int *a, int *b, int *c)
 {
-    
+    printf("Enter first number: ");
+    scanf("%d", a);
+    printf("Enter second number: ");
+    scanf("%d", b);
+    printf("Enter third number: ");
+    scanf("%d", c);
+}
+
+void compare(int a, int b, int c, int *largest)
+{
+    *largest = a;
+    if (b > *largest)
+    {
+        *largest = b;
+    }
+    if (c > *largest)
+    {
+        *largest = c;
+    }
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest number amongst %d, %d, and %d is %d\n", a, b, c, largest);
 }
