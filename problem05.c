acbe60a25e7849ec25cb1527004e9b7b2ded4b3f
aarav <aaravk.bsc23@rvu.edu.in>
2024-01-31 00:12:31 +0530

    test

--- problem05.c
+++ problem05.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+int input();
+int compare(int a,int b,int c);
+void output(int a,int b,int c, int largest);
+
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+
+
+}
+int input()
+{
+    int x;
+    printf("enter number: ");
+    scanf("%d",&x);
+    return x;
+}
+int compare(int a,int b, int c)
+{
+    int largest;
+    if(a>=b && a>=c)
+    {
+        largest=a;
+    }
+    if(b>=a && b>=c)
+    {
+        largest=b;
+    }
+    if (c>=a && c>=b)
+    {
+        largest=c;
+    }
+    return largest;
+}
+void output(int a,int b,int c, int largest)
+{
+    printf("the largest number amongst %d,%d and %d is %d",a,b,c,largest);
+}
---
+++ problem06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+
+int main()
+{
+
+}
+int input(int *a, int *b, int *c)
+{
+    
+}
